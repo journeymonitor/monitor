3a3d297aa83389a4f0a29135ff46e8475c2aa704
Manuel Kiessling <manuel@kiessling.net>
2015-05-11 12:46:36 +0200

    Added repo class

---
+++ src/Selenior/Monitor/Base/TestcaseRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Selenior\Monitor\Base;
+
+class TestcaseRepository {
+
+    private $dbConnection;
+
+    public function __construct(\PDO $dbConnection) {
+        $this->dbConnection = $dbConnection;
+        $dbConnection->exec("CREATE TABLE IF NOT EXISTS testcases (
+                             id TEXT PRIMARY KEY,
+                             userId TEXT,
+                             cadence TEXT,
+                             script TEXT)");
+    }
+
+    public function add(TestcaseModel $testcaseModel) {
+        $this->remove($testcaseModel);
+        $sql = 'INSERT INTO testcases (id, userId, cadence, script) VALUES (:id, :userId, :cadence, :script)';
+        $stmt = $this->dbConnection->prepare($sql);
+
+        $stmt->bindValue(':id', $testcaseModel->getId());
+        $stmt->bindValue(':userId', $testcaseModel->getUserId());
+        $stmt->bindValue(':cadence', $testcaseModel->getCadence());
+        $stmt->bindValue(':script', $testcaseModel->getScript());
+
+        $stmt->execute();
+    }
+
+    private function remove(TestcaseModel $testcaseModel) {
+        $sql = 'DELETE FROM testcases WHERE id = :id';
+        $stmt = $this->dbConnection->prepare($sql);
+
+        $stmt->bindValue(':id', $testcaseModel->getId());
+
+        $stmt->execute();
+    }
+
+}
