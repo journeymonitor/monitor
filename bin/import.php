579d2de0b2ba560fc488adda8c715e5624269e26
Manuel Kiessling <manuel@kiessling.net>
2016-04-08 15:48:27 +0200

    Added missing logger injection for importer.

--- bin/import.php
+++ bin/import.php
@@ -8,6 +8,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use GuzzleHttp\Client;
 use JourneyMonitor\Monitor\Base\TestcaseRepository;
 use JourneyMonitor\Monitor\Base\EnvironmentInfo;
+use JourneyMonitor\Monitor\Base\Logger;
 use JourneyMonitor\Monitor\TestcaseImporter\ScriptTransformer;
 use JourneyMonitor\Monitor\TestcaseImporter\Importer;
 
@@ -19,7 +20,9 @@ $endpoint = $argv[1];
 $environmentInfo = new EnvironmentInfo();
 $environmentName = $environmentInfo->getName();
 
-$testcaseRepository = new TestcaseRepository(new PDO('sqlite:/var/tmp/journeymonitor-monitor-' . $environmentName . '.sqlite3'));
+$logger = new Logger();
+
+$testcaseRepository = new TestcaseRepository(new PDO('sqlite:/var/tmp/journeymonitor-monitor-' . $environmentName . '.sqlite3'), $logger);
 
 $importer = new Importer(new Client(), $endpoint, $testcaseRepository, new ScriptTransformer());
 $importer->run();
