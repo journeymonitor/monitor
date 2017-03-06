a088b430a9453ea3da71b588f96f6ae8475e1fce
Manuel Kiessling <manuel@kiessling.net>
2017-03-06 07:48:11 +0100

    #19: Moved getUrlsOfRequestedPages from control to monitor.

--- src/JourneyMonitor/Monitor/JobRunner/LogAnalyzer.php
+++ src/JourneyMonitor/Monitor/JobRunner/LogAnalyzer.php
@@ -29,4 +29,31 @@ class LogAnalyzer
         }
         return false;
     }
+
+    /**
+     * Get URLs of the pages that where visited during the journey
+     *
+     * This retrieves a list of the "main" web pages that were requested during the testcase run. The "main" pages are
+     * those that the Selenium script navigated to, not secondary HTTP requests which resulted from these page loads.
+     *
+     * @param string $logContent
+     * @return Array|String Array of requested URLs
+     */
+    public function getUrlsOfRequestedPages($logContent)
+    {
+        // [2015-05-28 23:46:34.933 +02:00] [INFO] - [Success] URL: [https://www.galeria-kaufhof.de/search?q=hose&page=4] / Title: [Suchergebnis f?r hose | GALERIA Kaufhof]
+        $matches = [];
+        $urls = [];
+        $lines = explode("\n", $logContent);
+        foreach ($lines as $line) {
+            if (strstr($line, '] [INFO] - [Success] URL: [')) {
+                preg_match('/\] \[INFO\] - \[Success\] URL: \[(.*?)\]/', $line, $matches);
+                if (array_key_exists(1, $matches)) {
+                    $urls[] = $matches[1];
+                }
+            }
+        }
+        return $urls;
+    }
+
 }
